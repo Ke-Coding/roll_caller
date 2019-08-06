590000e9dbd8b0847f4b1dd04db1c9acd32e81d2
Kevin00 <K_code@163.com>
2019-08-06 23:33:07 +0800

    Create config.h

---
+++ src/headers/config.h
@@ -0,0 +1,12 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define GROUP_COUNT 5
+#define CLASS_COUNT 10
+#define CLASS_ROLL_NAMES_COUNT 5
+#define UPDATE_INTERVAL 25
+
+using vint = int;
+using xint = unsigned int;
+
+#endif // CONFIG_H
