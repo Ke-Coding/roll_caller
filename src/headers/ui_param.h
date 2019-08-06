2fc5c094c5cb8524c4ca582bf6f490a661f4b648
Kevin00 <K_code@163.com>
2019-08-06 23:33:37 +0800

    Create ui_param.h

---
+++ src/headers/ui_param.h
@@ -0,0 +1,19 @@
+#ifndef UI_PARAM_H
+#define UI_PARAM_H
+
+enum ModeEnum
+{
+	GROUP_MODE,
+	CLASS_MODE,
+	MODE_CNT
+};
+
+enum ModeIndex
+{
+	GROUP_MODE_INDEX,
+	CLASS_MODE_INDEX,
+	MODE_INDEX_CNT
+};
+
+
+#endif // UI_PARAM_H
